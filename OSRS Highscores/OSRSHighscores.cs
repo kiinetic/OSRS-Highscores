b4468b5c50daf929974fd56b39bdf6a7de80fe75
kiinetic <xitsicex@gmail.com>
2015-10-20 14:33:42 -0600

    Initial commit

---
+++ OSRS Highscores/OSRSHighscores.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+
+namespace OSRS_Highscores {
+    public partial class OSRSHighscores : Form {
+        public OSRSHighscores() {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false; // ignore illegal cross threaded calls for simplicity
+        }
+        const int STEP_SIZE = 10;
+
+        List<Player> players = new List<Player>();
+
+        Stopwatch sw;
+
+        int threadsCompleted;
+        int start;
+        int playersCompleted;
+
+        private void button1_Click(object sender, EventArgs e) {
+            sw = new Stopwatch();
+            sw.Start();
+            for(int i = 0; i < 5; i++) {
+                BackgroundWorker bg = new BackgroundWorker();
+                bg.DoWork += bg_DoWork;
+                bg.RunWorkerCompleted += bg_RunWorkerCompleted;
+                bg.RunWorkerAsync(start);
+                start = start + STEP_SIZE;
+            }
+        }
+
+        void bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            threadsCompleted++;
+            listBox1.Items.Add(threadsCompleted);
+            if(threadsCompleted == 5) { // 1 thread per 10 pages is fine, curious to see the performance of maybe 1 thread per page
+                sw.Stop();
+                listBox1.Items.Add(sw.Elapsed.Seconds);
+                listBox1.Items.Add(players.Count);
+            }
+        }
+
+        void bg_DoWork(object sender, DoWorkEventArgs e) {
+            int end = (int)e.Argument + STEP_SIZE;
+            int start = (int)e.Argument;
+            List<int> tempStats = new List<int>();
+            for(int m = start; m < end; m++) {
+                string[] dataSplit;
+                WebRequest request = WebRequest.Create("http://services.runescape.com/m=hiscore_oldschool/overall.ws?table=0&page=" + m);
+                request.Proxy = null; // this apparently helps speed the process up a bit... unsure of its effectiveness
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string strResponse = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
+                string[] userData;
+                dataSplit = strResponse.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                for(int i = 0; i < dataSplit.Length; i++) { // run on a different thread to complete faster?
+                    if(dataSplit[i].Contains("hiscorepersonal.ws?user1=")) {
+                        int startIndex = dataSplit[i].IndexOf('>') + 1;
+                        int endIndex = dataSplit[i].LastIndexOf('<');
+                        string name = dataSplit[i].Substring(startIndex, endIndex - startIndex);
+                        string replacedName = name.Replace("�", "%20"); // reading pure html so we're going to run into issues like this
+                        request = WebRequest.Create("http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" + replacedName);
+                        response = request.GetResponse();
+                        dataStream = response.GetResponseStream();
+                        reader = new StreamReader(dataStream);
+                        strResponse = reader.ReadToEnd();
+                        userData = strResponse.Split(',');
+                        response.Close();
+                        reader.Close();
+                        for(int l = 0; l < userData.Length; l++) {
+                            if(l > 2 && l % 2 != 0 && !userData[l].Contains("-1") && l < 48) // browser renders junk
+                                tempStats.Add(int.Parse(userData[l]));
+                        }
+                        players.Add(new Player(tempStats[0], tempStats[1], tempStats[2], tempStats[3], tempStats[4], tempStats[5], tempStats[6], tempStats[7], tempStats[8], tempStats[9],
+                            tempStats[10], tempStats[11], tempStats[12], tempStats[13], tempStats[14], tempStats[15], tempStats[16], tempStats[17], tempStats[18], tempStats[19], tempStats[20],
+                            tempStats[21], tempStats[22], replacedName));
+                        tempStats.Clear();
+                        playersCompleted++;
+                        listBox1.Items.Add(playersCompleted + " players completed");
+                    }
+                }
+                listBox1.Items.Add("page " + m + " done");
+            }
+
+
+
+
+            //Stopwatch stopWatch = new Stopwatch();
+            //List<int> tempStats = new List<int>();
+            //string[] dataSplit;
+            //int numOfPlayers = 0;
+            //WebRequest request = WebRequest.Create("http://services.runescape.com/m=hiscore_oldschool/overall.ws?table=0&page=" + e.Argument);
+            //request.Proxy = null;
+            //WebResponse response = request.GetResponse();
+            //Stream dataStream = response.GetResponseStream();
+            //StreamReader reader = new StreamReader(dataStream);
+            //string strResponse = reader.ReadToEnd();
+            //reader.Close();
+            //response.Close();
+            //string[] userData;
+            //dataSplit = strResponse.Split(new char[] {'\n'}, StringSplitOptions.RemoveEmptyEntries);
+            //for(int i = 0; i < dataSplit.Length; i++) {
+            //    if(dataSplit[i].Contains("hiscorepersonal.ws?user1=")) {
+            //        stopWatch.Start();
+            //        int startIndex = dataSplit[i].IndexOf('>') + 1;
+            //        int endIndex = dataSplit[i].LastIndexOf('<');
+            //        string name = dataSplit[i].Substring(startIndex, endIndex - startIndex);
+            //        string test = name.Replace("�", "%20");
+            //        request = WebRequest.Create("http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" + test);
+            //        response = request.GetResponse();
+            //        dataStream = response.GetResponseStream();
+            //        reader = new StreamReader(dataStream);
+            //        strResponse = reader.ReadToEnd();
+            //        userData = strResponse.Split(',');
+            //        response.Close();
+            //        reader.Close();
+            //        for(int l = 0; l < userData.Length; l++) {
+            //            if(l > 2 && l % 2 != 0 && !userData[l].Contains("-1") && l < 48) // browser renders crap 
+            //                tempStats.Add(int.Parse(userData[l]));
+            //        }
+            //        players.Add(new Player(tempStats[0], tempStats[1], tempStats[2], tempStats[3], tempStats[4], tempStats[5], tempStats[6], tempStats[7], tempStats[8], tempStats[9],
+            //            tempStats[10], tempStats[11], tempStats[12], tempStats[13], tempStats[14], tempStats[15], tempStats[16], tempStats[17], tempStats[18], tempStats[19], tempStats[20],
+            //            tempStats[21], tempStats[22], test));
+            //        stopWatch.Stop();
+            //        listBox1.Items.Add("took " + stopWatch.Elapsed.Milliseconds + " ms to complete person " + numOfPlayers);
+            //        numOfPlayers++;
+            //        tempStats.Clear();
+
+            //    }
+            //}
+        }
+    }
+}
---
+++ OSRS Highscores/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSRS_Highscores {
+    class Player {
+
+        public int attack, defence, strength, hitpoints, range, prayer, magic, cooking, woodcutting, fletching, fishing, firemaking, crafting, smithing, mining, herblore, agility, thieving, slayer, farming, runecrafting, hunter, construction;
+        public string playerName;
+
+        public Player(int attack, int defence, int strength, int hitpoints, int range, int prayer, int magic, int cooking, int woodcutting, int fletching, int fishing,
+            int firemaking, int crafting, int smithing, int mining, int herblore, int agility, int thieving, int slayer, int farming, int runecrafting, int hunter,
+            int construction, string playerName) {
+                this.attack = attack;
+                this.defence = defence;
+                this.strength = strength;
+                this.hitpoints = hitpoints;
+                this.range = range;
+                this.prayer = prayer;
+                this.magic = magic;
+                this.cooking = cooking;
+                this.woodcutting = woodcutting;
+                this.fletching = fletching;
+                this.fishing = fishing;
+                this.firemaking = firemaking;
+                this.crafting = crafting;
+                this.smithing = smithing;
+                this.mining = mining;
+                this.agility = agility;
+                this.thieving = thieving;
+                this.slayer = slayer;
+                this.farming = farming;
+                this.runecrafting = runecrafting;
+                this.hunter = hunter;
+                this.construction = construction;
+                this.herblore = herblore;
+                this.playerName = playerName;
+        }
+    }
+}
